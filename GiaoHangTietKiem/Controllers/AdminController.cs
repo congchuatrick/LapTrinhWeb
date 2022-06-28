f216186877bbddecc94f3ad629207b0579241173
Nguyễn Nhựt Trường <91083231+congchuatrick@users.noreply.github.com>
2022-06-28 16:41:19 +0700

    AdminController
    Phòng ban
    Hàng hoá
    Tuyên dương

--- GiaoHangTietKiem/Controllers/AdminController.cs
+++ GiaoHangTietKiem/Controllers/AdminController.cs
@@ -312,5 +312,209 @@ namespace GiaoHangTietKiem.Controllers
 
             return View(nk);
         }
+
+        //----------CÁC MẶT HÀNG----------
+        [HttpGet]
+        public ActionResult IndexGoods()
+        {
+            var listGoods = context.HangHoas.ToList();
+            return View(listGoods);
+        }
+
+        [HttpGet]
+        public ActionResult CreateGoods()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateGoods(HangHoa hh)
+        {
+            context.HangHoas.Add(hh);
+            context.SaveChanges();
+
+            return RedirectToAction("IndexGoods", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult DetailsGoods(string id)
+        {
+            HangHoa hh = context.HangHoas.FirstOrDefault(r => r.MaHH == id);
+            if (hh == null)
+                return HttpNotFound();
+            return View(hh);
+        }
+
+        [HttpGet]
+        public ActionResult EditGoods(string id)
+        {
+            HangHoa hh = context.HangHoas.FirstOrDefault(r => r.MaHH == id);
+            if (hh == null)
+                return HttpNotFound();
+            return View(hh);
+        }
+        [HttpPost]
+        public ActionResult EditGoods(HangHoa hh)
+        {
+            HangHoa dbUpdate = context.HangHoas.FirstOrDefault(r => r.MaHH == hh.MaHH);
+            if (dbUpdate != null)
+            {
+                dbUpdate.TenHH = hh.TenHH;
+                dbUpdate.MoTa = hh.MoTa;
+                dbUpdate.DonVT = hh.DonVT;
+                dbUpdate.MaLHH = hh.MaLHH;
+                dbUpdate.GiaTien = hh.GiaTien;
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("IndexGoods", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteGoods(string id)
+        {
+            HangHoa hh = context.HangHoas.FirstOrDefault(r => r.MaHH == id);
+            if (hh != null)
+            {
+                context.HangHoas.Remove(hh);
+                context.SaveChanges();
+            }
+
+            return View(hh);
+        }
+
+        //----------TUYÊN DƯƠNG----------
+        [HttpGet]
+        public ActionResult IndexCommend()
+        {
+            var listCommend = context.TuyenDuongs.ToList();
+            return View(listCommend);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCommend()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCommend(TuyenDuong td)
+        {
+            context.TuyenDuongs.Add(td);
+            context.SaveChanges();
+
+            return RedirectToAction("IndexCommend", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult DetailsCommend(string id)
+        {
+            TuyenDuong td = context.TuyenDuongs.FirstOrDefault(r => r.MaTD == id);
+            if (td == null)
+                return HttpNotFound();
+            return View(td);
+        }
+
+        [HttpGet]
+        public ActionResult EditCommend(string id)
+        {
+            TuyenDuong td = context.TuyenDuongs.FirstOrDefault(r => r.MaTD == id);
+            if (td == null)
+                return HttpNotFound();
+            return View(td);
+        }
+        [HttpPost]
+        public ActionResult EditCommend(TuyenDuong td)
+        {
+            TuyenDuong dbUpdate = context.TuyenDuongs.FirstOrDefault(r => r.MaTD == td.MaTD);
+            if (dbUpdate != null)
+            {
+                dbUpdate.TenTD = td.TenTD;
+                dbUpdate.ThoiGian = td.ThoiGian;
+                dbUpdate.MaKhoBD = td.MaKhoBD;
+                dbUpdate.MaKhoKT = td.MaKhoKT;
+                dbUpdate.MaLVC = td.MaLVC;
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("IndexCommend", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteCommend(string id)
+        {
+            TuyenDuong td = context.TuyenDuongs.FirstOrDefault(r => r.MaTD == id);
+            if (td != null)
+            {
+                context.TuyenDuongs.Remove(td);
+                context.SaveChanges();
+            }
+
+            return View(td);
+        }
+
+        //----------PHÒNG BAN----------
+        [HttpGet]
+        public ActionResult IndexDepartment()
+        {
+            var listDepartment = context.PhongBans.ToList();
+            return View(listDepartment);
+        }
+
+        [HttpGet]
+        public ActionResult CreateDepartment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateDepartment(PhongBan pb)
+        {
+            context.PhongBans.Add(pb);
+            context.SaveChanges();
+
+            return RedirectToAction("IndexDepartment", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult DetailsDepartment(string id)
+        {
+            PhongBan pb = context.PhongBans.FirstOrDefault(r => r.MaPB == id);
+            if (pb == null)
+                return HttpNotFound();
+            return View(pb);
+        }
+
+        [HttpGet]
+        public ActionResult EditDepartment(string id)
+        {
+            PhongBan pb = context.PhongBans.FirstOrDefault(r => r.MaPB == id);
+            if (pb == null)
+                return HttpNotFound();
+            return View(pb);
+        }
+        [HttpPost]
+        public ActionResult EditDepartment(PhongBan pb)
+        {
+            PhongBan dbUpdate = context.PhongBans.FirstOrDefault(r => r.MaPB == pb.MaPB);
+            if (dbUpdate != null)
+            {
+                dbUpdate.TenPB = pb.TenPB;
+                dbUpdate.NhiemVu = pb.NhiemVu;
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("IndexDepartment", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteDepartment(string id)
+        {
+            PhongBan pb = context.PhongBans.FirstOrDefault(r => r.MaPB == id);
+            if (pb != null)
+            {
+                context.PhongBans.Remove(pb);
+                context.SaveChanges();
+            }
+
+            return View(pb);
+        }
     }
 }
